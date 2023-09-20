cfd13c26310ffcdc854888fcfe98ba416d5d6853
Nwalozie <iyekcnwalozie@gmail.com>
2023-09-19 20:57:24 -0700

    updates

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- Check if the database hbnb_test_db exists
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Check if the user hbnb_test exists
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
