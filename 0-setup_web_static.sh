3f65c1d700426fa80c472970043708121b11a3b9
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-04 14:36:45 -0700

    Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,27 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-if ! command -v nginx &> /dev/null
-then
-	sudo apt update
-	sudon apt install -y nginx
-fi
-
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
-
-sudo echo "
-<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
-  <head/>
+  </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-config=$(cat <<EOL
-server {
-    listen 80;
-    server_name _;
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-}
-EOL
-)
-
-echo "$config" | sudo tee /etc/nginx/sites-available/default > /dev/null
-
-sudo systemctl restart nginx
+sudo service nginx restart
---
+++ 0-setup_web_statics.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+
+if ! command -v nginx &> /dev/null
+then
+	sudo apt update
+	sudon apt install -y nginx
+fi
+
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+
+sudo echo "
+<html>
+  <head>
+  <head/>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+config=$(cat <<EOL
+server {
+    listen 80;
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}
+EOL
+)
+
+echo "$config" | sudo tee /etc/nginx/sites-available/default > /dev/null
+
+sudo systemctl restart nginx
