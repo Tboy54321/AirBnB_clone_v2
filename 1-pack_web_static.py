b67d23a46ac507012ed49066e982b00610f4b6f9
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-05 15:17:08 -0700

    updated compressed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive"""
-from fabric.api import local, runs_once
-from datetime import datetime
+"""A module for Fabric script that generates a .tgz archive."""
 import os
+from datetime import datetime
+from fabric.api import local, runs_once
 
 
 @runs_once
 def do_pack():
-    """Archiving the static files"""
-    now = datetime.now()
-    formatted_time = now.strftime("%Y%m%d%H%M%S")
+    """Archives the static files."""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
-    archive_name = "web_static_{}.tgz".format(formatted_time)
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            d_time.year,
+            d_time.month,
+            d_time.day,
+            d_time.hour,
+            d_time.minute,
+            d_time.second
+            )
     try:
-        print("Packing web_static to {}".format(archive_name))
-        local("tar -cvzf {} web_static".format(archive_name))
-        size = os.stat(archive_name).st_size
-        print("web_static packed: {} -> {} Bytes".format(archive_name, size))
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
     except Exception:
-        archive_name = None
-    return archive_name
+        output = None
+    return output
