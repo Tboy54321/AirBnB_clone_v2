68b5ac1fb123e10c0c26abbcb1e2f7a0bb315923
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-06 13:13:33 -0700

    updated tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,6 +20,27 @@ sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root   /var/www/html;
+    index  index.html index.htm;
+    add_header X-Served-By $HOSTNAME;
+
+    location /redirect_me {
+        return 301 http://youtube.com/;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 sudo service nginx restart
