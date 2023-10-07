b2a722759fd449394f79f6b717518392d1318a25
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-07 14:32:19 -0700

    deployment of web static pages

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,23 +7,17 @@ from fabric.api import local, runs_once
 
 @runs_once
 def do_pack():
-    """Archives the static files."""
+    """Archiving all web static files file"""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
-    d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-            d_time.year,
-            d_time.month,
-            d_time.day,
-            d_time.hour,
-            d_time.minute,
-            d_time.second
-            )
+    now = datetime.now()
+    formatted_time = now.strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_{}.tgz".format(formatted_time)
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
+        print("Packing web_static to {}".format(archive_name))
+        local("tar -cvzf {} web_static".format(archive_name))
+        size = os.stat(archive_name).st_size
+        print("web_static packed: {} -> {} Bytes".format(archive_name, size))
     except Exception:
-        output = None
-    return output
+        archive_name = None
+    return archive_name
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
-
+"""A module for Fabric script that generates a .tgz archive."""
+import os
 from datetime import datetime
 from fabric.api import *
-import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["18.233.64.67","52.4.93.199"]
 env.user = "ubuntu"
 
 
+@runs_once
 def do_pack():
-    """
-        return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
-
+    """Archiving all web static files file"""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    now = datetime.now()
+    formatted_time = now.strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_{}.tgz".format(formatted_time)
+    try:
+        print("Packing web_static to {}".format(archive_name))
+        local("tar -cvzf {} web_static".format(archive_name))
+        size = os.stat(archive_name).st_size
+        print("web_static packed: {} -> {} Bytes".format(archive_name, size))
+    except Exception:
+        archive_name = None
+    return archive_name
 
 def do_deploy(archive_path):
-    """
-        Distribute archive.
-    """
+    """Deployment"""
     if os.path.exists(archive_path):
         archived_file = archive_path[9:]
         newest_version = "/data/web_static/releases/" + archived_file[:-4]
