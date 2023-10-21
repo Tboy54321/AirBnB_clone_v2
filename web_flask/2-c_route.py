ed12850fc6a5864b34fe856b6b7f4381c2e1ce4f
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-21 16:25:32 -0700

    Adding variablesto my route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,5 +20,11 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """Function that accepts Argument"""
+    return "C {}".format(text.replace("_", " "))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
