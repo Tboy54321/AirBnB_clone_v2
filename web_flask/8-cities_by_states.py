d42b98c7f3be6c3da7348900e701ce1a304d160e
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-10-24 15:04:41 -0700

    Displays the cities by the states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script that displays and the render the sstates"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def display_states():
+    states = sorted(storage.all('State').values(), key=lambda i: i.name)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def close_strorage(self):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
